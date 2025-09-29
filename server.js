4ec8697c157a0a9352a8d85af6a90348ad015651
Maira Kotsovoulou <mkotsovoulou@byte.gr>
2025-09-29 18:24:41 +0300

    flash api added

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const app = express();
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, "public")));
 
+
+// API endpoint to serve flashcards.json
+app.get("/api/flashcards", (req, res) => {
+  fs.readFile(path.join(__dirname, "flashcards.json"), "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Unable to load flashcards" });
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+
 // Fallback to index.html (in case you’re using a single page app)
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
